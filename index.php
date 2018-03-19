ba2693e8be7f2aec6a4abf5c40d4f5244ef59329
Harrison <34606511+Seralnl@users.noreply.github.com>
2018-03-19 12:31:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+    <title>Studenten Verhuis Service</title>
+
+    <link href="/stylesheet/css/Bootstrap.css" rel="stylesheet">
+    <link href="/stylesheet/css/Style.css" rel="stylesheet">
+</head>
+<body>
+	<nav class="navigation">
+		<div class="navigation-items">
+			<div class="container">
+				<div class="navbar-brand">Titel</div>
+				
+				<label for="drop" class="toggle">&#9776;</label>
+				<input type="checkbox" id="drop" />
+				
+				<ul class="links">
+					<div class="dropdown">
+						Dropdown
+						<div class="items">
+							<a href="/">Item 1</a>
+							<a href="/">Item 2</a>
+							<div class="divider"></div>
+							<a href="/">Item 3</a>
+						</div>
+					</div>
+					<a href="/" class="active">Home</a>
+					<a href="/" class="active">About</a>
+					<div class="dropdown">
+						Dropdown
+						<div class="items">
+							<a href="/">Item 1</a>
+							<a href="/">Item 2</a>
+							<div class="divider"></div>
+							<a href="/">Item 3</a>
+						</div>
+					</div>
+					<a href="/contact">Contact</a>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<header class="top">
+		<section class="background">
+			<div class="content">
+				<p><strong>Lorem Ipsum</strong></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</section>
+		<section class="information">
+			<div class="content">
+				<div class="item">
+					<strong>Item 1</strong>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</div>
+				<div class="item">
+					<strong>Item 2</strong>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</div>
+				<div class="item">
+					<strong>Item 3</strong>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</div>
+			</div>
+		</section>
+	</header>
+	
+	<section class="news">
+		<div class="container">
+			<div class="col-md-4">
+				<a href="" class="card">
+					<img class="card-image" src="https://seralstudios.com/src/01jg894i87g455ujg43893044DJ83jf84D.png"/>
+					<div class="card-body">
+						<strong>Lorem ipsum</strong>
+						<small>On the 17th of august</small>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="" class="card">
+					<img class="card-image" src="https://seralstudios.com/src/01jg894i87g455ujg43893000DJ83jf84D.png"/>
+					<div class="card-body">
+						<strong>Lorem ipsum</strong>
+						<small>On the 17th of august</small>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="" class="card">
+					<img class="card-image" src="https://seralstudios.com/src/01jg894i87g455ujg43893022DJ83jf84D.png"/>
+					<div class="card-body">
+						<strong>Lorem ipsum</strong>
+						<small>On the 17th of august</small>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</a>
+			</div>
+		</div>
+	</section>
+	
+	<div style="height: 100px;"></div>
+	
+</body>
+</html>
